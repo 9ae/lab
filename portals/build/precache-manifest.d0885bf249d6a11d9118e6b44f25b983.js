1b9c9e7dd456609374f46d34390bcf4588c3af21
AQW <alice@valour.me>
2022-04-25 17:09:59 -0400

    keep build dir

---
+++ portals/build/precache-manifest.d0885bf249d6a11d9118e6b44f25b983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9152b3d9ab6d6cf3e0b2",
+    "url": "/static/css/main.1e19f160.chunk.css"
+  },
+  {
+    "revision": "9152b3d9ab6d6cf3e0b2",
+    "url": "/static/js/main.9152b3d9.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "8df1cdd108653860aa95",
+    "url": "/static/js/2.8df1cdd1.chunk.js"
+  },
+  {
+    "revision": "6911db604fedf1b3b856",
+    "url": "/static/js/runtime~main.6911db60.js"
+  },
+  {
+    "revision": "dc827f625c59bbd28f9835f1bd82bb07",
+    "url": "/index.html"
+  }
+];
