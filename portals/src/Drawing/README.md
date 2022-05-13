66fb2ddfe190316e8e6c2379ad21e9ad4e32e1a6
AQW <alice@valour.me>
2022-05-13 11:07:25 -0400

    touch drawing

---
+++ portals/src/Drawing/README.md
@@ -0,0 +1,23 @@
+# Goal: Implement ipad gestures on canvas with touch events
+
+# Create a drawing canvas with a redo & undo stack
+
+- [x] Make drawing canvas
+- [ ] Implement redo undo stack
+
+# Our command stack
+
+How do we implement our command stack?
+
+Option 1:
+i = n-1
+[....(.).]
+
+Option 2:
+history: [..]
+future: [..]
+and we draw from history, and push to future on undo
+
+5/13:
+Issue: Our drawing stack is not drawing. Why?
+-> We weren't pushing our arrays correct
---
+++ portals/src/Drawing/index.tsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useRef, useState } from "react";
+
+type Point = [number, number]
+
+interface Command {
+  path: Point[]
+}
+
+const width = 500;
+const height = 500;
+
+const Page = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // const [isDrawing, setIsDrawing] = useState<boolean>(false);
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const [drawingPath, setDrawingPath] = useState<Point[]>([]);
+  const [commandIndex, setCommandIndex] = useState<number>(-1);
+  const [commands, setCommands] = useState<Command[]>([]);
+  const [frame, setFrame] = useState(0)
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas === null) {
+      console.log('canvas not ready yet');
+      return;
+    }
+    const _ctx = canvas.getContext('2d');
+    if (!ctx && _ctx) {
+      setCtx(_ctx);
+    }
+
+    if (ctx && drawingPath.length === 0 && commandIndex >= 0) {
+      console.log('drawing');
+      ctx.clearRect(0, 0, width, height);
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 4;
+
+      for (let i = 0; i < commandIndex; i++) {
+        const path = commands[i].path;
+        if (path.length > 2) {
+          console.log('draw path', path);
+          ctx.beginPath();
+          ctx.moveTo(path[0][0], path[0][1]);
+          for (let j = 1; j < path.length; j++) {
+            ctx.lineTo(path[j][0], path[j][1]);
+          }
+          ctx.closePath();
+          ctx.stroke();
+        }
+      }
+
+
+    }
+
+  }, [commandIndex]);
+
+  const onTouchStart = (evt: React.TouchEvent) => {
+    console.log('touch start', evt)
+    if (ctx && evt.touches.length > 0) {
+      const finger = evt.touches[0];
+      ctx.beginPath();
+      ctx.moveTo(finger.clientX, finger.clientY);
+      ctx.strokeStyle = 'blue';
+      ctx.lineWidth = 1;
+
+      setDrawingPath([[finger.clientX, finger.clientY]])
+    }
+  };
+  const onTouchMove = (evt: React.TouchEvent) => {
+    console.log('touch move', evt)
+    if (ctx && evt.touches.length > 0 && drawingPath) {
+      const finger = evt.touches[0];
+      ctx.lineTo(finger.clientX, finger.clientY);
+      ctx.stroke();
+      const newPath = drawingPath.concat([[finger.clientX, finger.clientY]]);
+      setDrawingPath(newPath);
+    }
+  };
+  const onTouchEnd = (evt: React.TouchEvent) => {
+    console.log('touch end', evt)
+
+    if (ctx) {
+      ctx.closePath();
+      ctx.stroke();
+
+      const cmd = { path: [...drawingPath] }
+      setCommands(commands.concat([cmd]));
+      setCommandIndex(commandIndex + 1);
+
+      setDrawingPath([]);
+    }
+  };
+
+  const onUndo = (evt: React.MouseEvent) => {
+    if (commandIndex === 0) return;
+    setCommandIndex(commandIndex - 1);
+    console.log('undo', commandIndex);
+  }
+
+  const onRedo = (evt: React.MouseEvent) => {
+    if (commandIndex === commands.length - 1) return;
+    console.log('redo');
+    setCommandIndex(commandIndex + 1);
+    console.log('undo', commandIndex);
+  }
+
+  return (<>
+    <canvas width={width} height={height}
+      ref={canvasRef} style={{ border: "1px solid red" }}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    ></canvas>
+    <button onClick={onUndo} disabled={commandIndex === 0}>Undo</button>
+    <button onClick={onRedo} disabled={commandIndex === commands.length - 1}>Redo</button>
+  </>);
+}
+
+export default Page;
